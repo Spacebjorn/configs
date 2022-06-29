f832f62e7dafdc560e79e9d12a402ab633b9fe0e
Sebastian Olsen <30187002+Spacebjorn@users.noreply.github.com>
2022-06-29 12:08:18 +0200

    Updated script to support colemak layout

--- linux/scripts/layout_switch.sh
+++ linux/scripts/layout_switch.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 # If an explicit layout is provided as an argument, use it. Otherwise, select the next layout from
-# the set [us, dk].
+# the set [us, it, fr].
 if [[ -n "$1" ]]; then
     setxkbmap $1
-else
-    layout=$(setxkbmap -query | awk '/layout/ {print $2}')
+else                          # Im sorry Jesus
+    layout=$(setxkbmap -query | awk '{ if(/variant/) {variant=$2} else if ($1 ~ /layout/) layout=$2} END{print variant ? variant : layout}')
     case $layout in
         us)
                 setxkbmap dk
             ;;
         dk)
-                setxkbmap us -variant euro
+                setxkbmap us -variant colemak
+            ;;
+        colemak)
+                setxkbmap us
+            ;;
+        *)
+                setxkbmap us
             ;;
     esac
 fi
